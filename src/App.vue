2a85226683bd855790e3b8f9e99bd3af6c1fe6d1
Rick Bordelon <rick.bordelon@redsailtechnologies.com>
2026-05-18 11:46:17 -0500

    Initial page

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ThemeToggle from './components/ThemeToggle.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center relative">
+    <ThemeToggle class="absolute top-6 right-6" />
+    <h1
+      class="text-5xl sm:text-7xl md:text-8xl lg:text-9xl font-black uppercase tracking-tighter leading-none text-center select-none"
+      style="font-family: 'Arial Black', 'Helvetica Neue', sans-serif; letter-spacing: -0.04em;"
+    >
+      Rick<br/>Bordelon
+    </h1>
+  </div>
+</template>
